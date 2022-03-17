04493d050d9fedd0048a9172997cbc5ad7e1a6bb
Savser91 <savser91@gmail.com>
2022-03-17 19:24:29 +0300

    changed list to set as set prevents duplicates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,14 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        String words = "Rdfad Asd df erty qwerty Koaty jhui rest motad q agoka polka gel tjadf";
+        String words = "Rdfad Asd df erty qwerty Koaty jhui rest erty motad q agoka polka gel tjadf";
 
-        Function<String, List<String>> dictionary = a ->
+        Function<String, Set<String>> dictionary = a ->
                 Arrays.stream(
                         a
                         .toLowerCase(Locale.ROOT)
                         .split(" "))
-                        .collect(Collectors.toList());
+                        .collect(Collectors.toSet());
 
         System.out.println("Словарь: ");
 
